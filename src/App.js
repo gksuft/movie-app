58b9f0f440155a9fddd7c94a389088e1223c13b4
Anna <gksuft098@gmail.com>
2022-05-04 15:35:31 +0300

    Route change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Header></Header>
         <div className="container">
           <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/movie-app/' element={<Home/>} />
             <Route path="/movie/:imdbID" element={<MovieDetail/>} />
             <Route path="*" element={<PageNotFound/>} />
           </Routes>
